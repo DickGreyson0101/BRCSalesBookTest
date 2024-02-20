bffcc591ad24ee73553e57cebe12054bda0fbf81
AzureAD\LinhPhanThi <20520615@gm.uit.edu.vn>
2024-02-20 13:37:45 +0700

    time until report appeared

--- SpecFlowTestNet4_8/Components/UrlComponent.cs
+++ SpecFlowTestNet4_8/Components/UrlComponent.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using SalesbookTest.Contracts;
+using SalesbookTest.Drivers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,35 @@ using System.Threading.Tasks;
 
 namespace SalesbookTest.Components
 {
-    public class UrlComponent
+    public class UrlComponent : IBaseComponent
     {
+        private readonly By _locator;
+        private IWebElement _parent;
+        public UrlComponent(By locator)
+        {
+            _locator = locator;
 
+        }
+
+        public UrlComponent(string inputElementName)
+        {
+            //_parent = parent;
+            _locator = By.XPath($"//*[text()='{inputElementName}']");
+            //_locator = By.XPath($"(.//*[text()='{inputElementName}']/following::input[{inputIndex}])[1]");
+
+        }
+        public IWebElement GetInstance()
+        {
+            return BrcWebdriver.GetInstance().FindElement(_locator);
+        }
+        public IWebElement GetElement()
+        {
+            return GetInstance().FindElement(_locator);
+        }
+
+        public bool CheckDisplay()
+        {
+            return GetInstance().Displayed;
+        }
     }
 }
